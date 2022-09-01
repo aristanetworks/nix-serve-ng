e686b2b1fc1a84f54d3ac4895aaa2932c2719476
Gabriella Gonzalez <gabriel@arista.com>
2022-09-01 12:36:08 -0700

    Add `README.md` (#4)
    … and make matching changes to `flake.nix` to support the usage
    documented in the `README`

--- flake.nix
+++ flake.nix
@@ -10,60 +10,87 @@
   };
 
   outputs = { nixpkgs, utils, ... }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        overlay = pkgsNew: pkgsOld: {
-          haskellPackages = pkgsOld.haskellPackages.override (old: {
-            overrides =
-              pkgsNew.lib.fold pkgsNew.lib.composeExtensions (old.overrides or (_: _: { })) [
-                (pkgsNew.haskell.lib.packageSourceOverrides {
-                  nix-serve-ng = ./.;
-                })
-                (haskellPackagesNew: haskellPackagesOld: {
-                  nix-serve-ng =
-                    pkgsNew.haskell.lib.overrideCabal
-                      haskellPackagesOld.nix-serve-ng
-                      (old: {
-                        executableSystemDepends = (old.executableSystemDepends or []) ++ [
-                          pkgsNew.boost.dev
-                        ];
-                      });
-                })
-              ];
-          });
+    let
+      overlay = pkgsNew: pkgsOld: {
+        haskellPackages = pkgsOld.haskellPackages.override (old: {
+          overrides =
+            pkgsNew.lib.fold pkgsNew.lib.composeExtensions (old.overrides or (_: _: { })) [
+              (pkgsNew.haskell.lib.packageSourceOverrides {
+                nix-serve-ng = ./.;
+              })
+              (haskellPackagesNew: haskellPackagesOld: {
+                nix-serve-ng =
+                  pkgsNew.haskell.lib.overrideCabal
+                    haskellPackagesOld.nix-serve-ng
+                    (old: {
+                      executableSystemDepends = (old.executableSystemDepends or []) ++ [
+                        pkgsNew.boost.dev
+                      ];
+                    });
+              })
+            ];
+        });
 
-          nix-serve-ng =
-            pkgsNew.haskell.lib.justStaticExecutables
-              pkgsNew.haskellPackages.nix-serve-ng;
-        };
+        nix-serve-ng =
+          pkgsNew.haskell.lib.justStaticExecutables
+            pkgsNew.haskellPackages.nix-serve-ng;
+      };
 
-        pkgs = import nixpkgs {
-          config = { };
+    in
+      utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs {
+            config = { };
 
-          overlays = [ overlay ];
+            overlays = [ overlay ];
 
-          inherit system;
-        };
-
-        inherit (pkgs) nix-serve-ng;
-
-      in
-        rec {
-          packages.default = nix-serve-ng;
-
-          defaultPackage = packages.default;
-
-          apps.default = {
-            type = "app";
-
-            program = "${nix-serve-ng}/bin/nix-serve-ng";
+            inherit system;
           };
 
-          defaultApp = apps.default;
+          inherit (pkgs) nix-serve-ng;
 
-          devShells.default =
-            (pkgs.haskell.lib.doBenchmark pkgs.haskellPackages.nix-serve-ng).env;
+        in
+          rec {
+            packages.default = nix-serve-ng;
 
-          devShell = devShells.default;
-        });
+            defaultPackage = packages.default;
+
+            apps.default = {
+              type = "app";
+
+              program = "${nix-serve-ng}/bin/nix-serve-ng";
+            };
+
+            defaultApp = apps.default;
+
+            devShells.default =
+              (pkgs.haskell.lib.doBenchmark pkgs.haskellPackages.nix-serve-ng).env;
+
+            devShell = devShells.default;
+          }) // rec {
+        overlays = {
+          # The default overlay only adds the exports for
+          # `pkgs.haskellPackages.nix-serve-ng` and `pkgs.nix-serve-ng`
+          default = [ overlay ];
+
+          # This overlay additionally overrides `pkgs.nix-serve` to refer to
+          # `pkgs.nix-serve-ng`
+          override =
+            let
+              replace = pkgsNew: pkgsOld: {
+                nix-serve = pkgsNew.nix-serve-ng;
+              };
+
+            in
+              [ overlay replace ];
+        };
+
+        # The NixOS module is a thin wrapper around the overlay to replace
+        # `nix-serve` with `nix-serve-ng`
+        #
+        # You can continue to use the old `services.nix-serve` NixOS options.
+        nixosModules.default = {
+          nixpkgs.overlays = overlays.default;
+        };
+      };
 }
